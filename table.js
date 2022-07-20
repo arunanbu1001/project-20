ce3f1ec20504b87e6033bffa0ab16b99e7cbf61d
arunanbu1001 <arunanbu10012001@gmail.com>
2022-07-20 18:01:18 +0530

    update

--- table.js
+++ table.js
@@ -3,11 +3,11 @@ var tablearea = document.getElementById('tablearea');
 var table = document.createElement('table');
 table.setAttribute("class", "bor")
 var tr = [];
-
+var td =[] 
 for (i = 0; i < 5; i++) {
     tr[i] = document.createElement("tr")
     for (j = 0; j < 8; j++) {
-        td = document.createElement("td")
+        td= document.createElement("td")
                
                 function box(photo, name, mobile, batch, git, referby,referby1) {
                     able( photo)
@@ -17,31 +17,28 @@ for (i = 0; i < 5; i++) {
                     able( git)
                     able( referby)
                     able( referby1)
-                    
-                    
-                    function able(n) {
-                        td.append(n)
-                            console.log(td)
-                    }
+                    // tr.forEach()
+                   
                 }
-               
+                  box("photo", "name", "mobile", "batch", "git", "referby","location")
+box("photo", "EDWINRAJ", "9361046327", "FSD-9", "zeddyy", "velmrgan-fsd8","chennai")  
+console.log(tr[i])
+console.log(i)
+box("photo", "francis", "9500252669", "FSD-09", "FRANCISANIMH", "G.srya fsd 04","pavoorchathram")
+
+                function able(n) {
+                    td.append(n)
+                        console.log(td)
+                }
                 tr[i].append(td)
+                
+                td.setAttribute("class","bor")
     }
+    tr[i].append(td)
     table.append(tr[i])
-    
-   
+//td.setAttribute("class","bor")
 
 }
-box("photo", "name", "mobile", "batch", "git", "referby","location")
-box("photo", "EDWINRAJ", "9361046327", "FSD-9", "zeddyy", "velmrgan-fsd8","chennai")  
-box("photo", "francis", "9500252669", "FSD-09", "FRANCISANIMH", "G.srya fsd 04","pavoorchathram")
-console.log(tr[i])
-
-
-//tr.forEach(able)
-
-
-
 
 //console.log(k)
 
