fea66052296b8e1c19bdc672a3ff9980068c086c
arunanbu1001 <arunanbu10012001@gmail.com>
2022-07-26 13:57:16 +0530

    update

--- table.js
+++ table.js
@@ -1,9 +1,9 @@
 var table = document.createElement('table');
 var tr = document.createElement('tr');
-var img = '<img src="/images/ACE.jpg" />';
+
 var arrheader = ['Photo', 'Name', 'Mobile', 'Batch','Git', 'Refby', 'Location', 'Placement_status'];
 var array = [{
-  Photo: 'ad',
+  img: 'image/arun.jpeg',
   Name: 'Mehvish',
   mobile: 'Ashiq',
   Batch: 'Female',
@@ -13,7 +13,7 @@ var array = [{
   placment_status:'proces'
 },
   {
-    Photo: 'afr',
+    img: 'image/clenton.jpg ',
     Name: 'Arun',
     mobile: 'Ashiq',
     Batch: 'Male',
@@ -23,7 +23,7 @@ var array = [{
     placment_status:'proces'
   },
   {
-    Photo: 1,
+    img: 'image/kumares.jpeg',
     Name: 'Mehvish',
     mobile: 'Ashiq',
     Batch: 'Female',
@@ -56,13 +56,15 @@ var array = [{
 
 for (var j = 0; j < arrheader.length; j++) {
   var th = document.createElement('th');  
+
   var text = document.createTextNode(arrheader[j]);  
   th.appendChild(text);
   tr.appendChild(th);
 }
 table.appendChild(tr);
 
-for (var i = 0; i < array.length; i++) {
+// for (var i = 0; i < array.length; i++) {
+  array.forEach((kumar)=>{
   var tr = document.createElement('tr');
 
   var td1 = document.createElement('td');
@@ -74,14 +76,27 @@ for (var i = 0; i < array.length; i++) {
   var td7 = document.createElement('td');
   var td8 = document.createElement('td');
 
-  var text1 = document.createTextNode(array[i].Photo);
-  var text2 = document.createTextNode(array[i].Name);
-  var text3 = document.createTextNode(array[i].mobile);
-  var text4 = document.createTextNode(array[i].Batch);
-  var text5 = document.createTextNode(array[i].Git);
-  var text6 = document.createTextNode(array[i].Refby);
-  var text7 = document.createTextNode(array[i].Location);
-  var text8 = document.createTextNode(array[i].placment_status);
+  var text1 = document.createElement('img');
+  text1.src=kumar.img;
+  td1.appendChild(text1);
+  
+  text1.setAttribute('class','image' );
+  var text2 = document.createTextNode(kumar.Name);
+  var text3 = document.createTextNode(kumar.mobile);
+  var text4 = document.createTextNode(kumar.Batch);
+  var text5 = document.createTextNode(kumar.Git);
+  var text6 = document.createTextNode(kumar.Refby);
+  var text7 = document.createTextNode(kumar.Location);
+  var text8 = document.createTextNode(kumar.placment_status);
+  
+
+
+
+
+
+  
+
+
 
   td1.appendChild(text1);
   td2.appendChild(text2);
@@ -104,18 +119,11 @@ for (var i = 0; i < array.length; i++) {
 
   table.setAttribute("border", "solid 1px");
   table.setAttribute('style', 'width: 100%');
-  tr.setAttribute('text_align', 'center');
-  td1.setAttribute('text-align', 'center');
-  td2.setAttribute('text-align', 'center');
-  td3.setAttribute('text-align', 'center');
-  td4.setAttribute('text-align', 'center');
-  td5.setAttribute('text-align', 'center');
-  td6.setAttribute('text-align', 'center');
-  td7.setAttribute('text-align', 'center');
-  td8.setAttribute('text-align', 'center');
+  tr.setAttribute('id', 'ce');
+    
    
   table.appendChild(tr);
-}
+})
 document.body.appendChild(table);
 
 
